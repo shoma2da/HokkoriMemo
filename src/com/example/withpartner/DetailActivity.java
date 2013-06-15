fd7ac41db188b6437310c32627a78bbf3a4d7865
shoma2da <marcos.shoichi.matsuda@gmail.com>
2013-06-15 11:45:02 +0900

    ほっこり追加イベントを実装

--- src/com/example/withpartner/DetailActivity.java
+++ src/com/example/withpartner/DetailActivity.java
@@ -2,10 +2,15 @@ package com.example.withpartner;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.withpartner.model.DetailData;
 import com.example.withpartner.model.SQLiteDetailData;
@@ -23,6 +28,7 @@ public class DetailActivity extends Activity {
         
         DetailData detailData = getDetailData();
         updateViews(detailData);
+        initViews(detailData);
     }
     
     private DetailData getDetailData() {
@@ -33,5 +39,31 @@ public class DetailActivity extends Activity {
     private void updateViews(DetailData detailData) {
         ((ImageView)findViewById(R.id.image_type_icon)).setImageResource(detailData.getImageResourceId());
         ((TextView)findViewById(R.id.text_type_title)).setText(detailData.getTitle());
+    }
+    
+    private void initViews(final DetailData detailData) {
+        final EditText editText = (EditText)findViewById(R.id.edittext_hokkori);
+        final View hokkoriAddButton = findViewById(R.id.button_hokkori_add);
+        hokkoriAddButton.setClickable(false);
+        
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (null == editText.getText() || "".equals(editText.getText().toString()) ) {
+                    hokkoriAddButton.setClickable(false);
+                } else {
+                    hokkoriAddButton.setClickable(true);
+                }
+            }
+            @Override public void beforeTextChanged(CharSequence s, int start, int count, int after) { }
+            @Override public void afterTextChanged(Editable s) { }
+        });
+        hokkoriAddButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(DetailActivity.this, "押された", Toast.LENGTH_SHORT).show();
+                detailData.addHokkoriList(editText.getText().toString());
+            }
+        });
     }
 }
